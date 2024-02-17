c1dd68a1eb875b71cb7e0104291b1707aee89a75
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-17 20:19:56 +0200

    add mobile nav component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,7 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
-
 const Home = () => {
   return (
     <div>
       <p>Home</p>
-
-      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
