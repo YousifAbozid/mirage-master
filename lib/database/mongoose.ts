66f13aef3dd1bf276741f04ff6f8ccbdec95dda2
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-19 08:21:33 +0200

    add connect to db util

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,31 @@
+import mongoose, { Mongoose } from 'mongoose'
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+interface MongooseConnection {
+  conn: Mongoose | null
+  promise: Promise<Mongoose> | null
+}
+
+let cached: MongooseConnection = (global as any).mongoose
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null }
+}
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn
+
+  if (!MONGODB_URI) throw new Error('MONGODB_URI is not defined')
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: 'mirage-master',
+      bufferCommands: false,
+    })
+
+  cached.conn = await cached.promise
+
+  return cached.conn
+}
