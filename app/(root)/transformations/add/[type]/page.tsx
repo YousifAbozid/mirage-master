11af06a74734e265c75af43c7a634d81f45e689a
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-24 10:39:56 +0200

    add transformations header

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import Header from '@/components/shared/Header'
+import { transformationTypes } from '@/constants'
 
-const AddTransformationTypePage = () => {
-	return <div>AddTransformationTypePage</div>
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+	const transformation = transformationTypes[type]
+
+	return (
+		<>
+			<Header title={transformation.title} subtitle={transformation.subTitle} />
+		</>
+	)
 }
 
 export default AddTransformationTypePage
