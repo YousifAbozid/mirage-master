441e51f4f715faa1ace4e2b21f208a272382c0b5
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-24 22:27:34 +0200

    add toaster

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
+import { Toaster } from '@/components/ui/toaster'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
@@ -10,6 +11,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 			<div className='root-container'>
 				<div className='wrapper'>{children}</div>
 			</div>
+
+			<Toaster />
 		</main>
 	)
 }
