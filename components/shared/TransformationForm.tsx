5e39b779afde0a3fe085c6d939ec201f60b35900
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-24 17:43:54 +0200

    add custom filed

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -15,6 +15,7 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { defaultValues } from '@/constants'
+import { CustomField } from './CustomField'
 
 export const formSchema = z.object({
 	title: z.string(),
@@ -55,23 +56,13 @@ const TransformationForm = ({
 	return (
 		<Form {...form}>
 			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
-				<FormField
+				<CustomField
 					control={form.control}
-					name='username'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Username</FormLabel>
-							<FormControl>
-								<Input placeholder='shadcn' {...field} />
-							</FormControl>
-							<FormDescription>
-								This is your public display name.
-							</FormDescription>
-							<FormMessage />
-						</FormItem>
-					)}
+					name='title'
+					formLabel='Image Title'
+					className='w-full'
+					render={({ field }) => <Input {...field} className='input-field' />}
 				/>
-				<Button type='submit'>Submit</Button>
 			</form>
 		</Form>
 	)
