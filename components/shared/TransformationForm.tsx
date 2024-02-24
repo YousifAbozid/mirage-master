ae92634bfdd994974b3451c4877fdc041b519865
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-24 22:21:49 +0200

    add media uploader component

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -31,6 +31,7 @@ import {
 import { CustomField } from './CustomField'
 import { useState, useTransition } from 'react'
 import { AspectRatioKey, debounce, deepMergeObjects } from '@/lib/utils'
+import MediaUploader from './MediaUploader'
 
 export const formSchema = z.object({
 	title: z.string(),
@@ -214,6 +215,32 @@ const TransformationForm = ({
 						)}
 					</div>
 				)}
+
+				<div className='media-uploader-field'>
+					<CustomField
+						control={form.control}
+						name='publicId'
+						className='flex size-full flex-col'
+						render={({ field }) => (
+							<MediaUploader
+								onValueChange={field.onChange}
+								setImage={setImage}
+								publicId={field.value}
+								image={image}
+								type={type}
+							/>
+						)}
+					/>
+
+					{/* <TransformedImage
+						image={image}
+						type={type}
+						title={form.getValues().title}
+						isTransforming={isTransforming}
+						setIsTransforming={setIsTransforming}
+						transformationConfig={transformationConfig}
+					/> */}
+				</div>
 
 				<div className='flex flex-col gap-4'>
 					<Button
