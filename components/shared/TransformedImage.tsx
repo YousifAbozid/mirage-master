174c95a08c7268c85685daf9f0aceac9dde96db9
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-25 16:16:07 +0200

    update transformed image

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,5 +1,7 @@
-import { dataUrl, debounce, getImageSize } from '@/lib/utils'
-import { CldImage } from 'next-cloudinary'
+'use client'
+
+import { dataUrl, debounce, download, getImageSize } from '@/lib/utils'
+import { CldImage, getCldImageUrl } from 'next-cloudinary'
 import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props'
 import Image from 'next/image'
 import React from 'react'
@@ -18,12 +20,15 @@ const TransformedImage = ({
 	) => {
 		e.preventDefault()
 
-		// download(getCldImageUrl({
-		//   width: image?.width,
-		//   height: image?.height,
-		//   src: image?.publicId,
-		//   ...transformationConfig
-		// }), title)
+		download(
+			getCldImageUrl({
+				width: image?.width,
+				height: image?.height,
+				src: image?.publicId,
+				...transformationConfig
+			}),
+			title
+		)
 	}
 
 	return (
